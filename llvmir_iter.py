d27a2ee5831c67aa5a2fa153808bf993a5e8db7e
max <maksim.levental@gmail.com>
2024-05-21 16:40:44 -0500

    do the bc thing

---
+++ llvmir_iter.py
@@ -0,0 +1,27 @@
+import llvmlite.binding as llvm
+
+
+with open("libMLIR-C.bc", "rb") as ir:
+    m = llvm.parse_bitcode(ir.read())
+
+for f in m.functions:
+    print(f'Function: {f.name}/`{f.type}`')
+    print(f'Function attributes: {list(f.attributes)}')
+    for a in f.arguments:
+        print(f'Argument: {a.name}/`{a.type}`')
+        print(f'Argument attributes: {list(a.attributes)}')
+    for b in f.blocks:
+        print(f'Block: {b.name}/`{b.type}`\n{b}\nEnd of Block')
+        for i in b.instructions:
+            print(f'Instruction: {i.name}/`{i.opcode}`/`{i.type}`: `{i}`')
+            print(f'Attributes: {list(i.attributes)}')
+            for o in i.operands:
+                print(f'Operand: {o.name}/{o.type}')
+
+    break
+
+for g in m.global_variables:
+    print(f'Global: {g.name}/`{g.type}`')
+    print(f'Attributes: {list(g.attributes)}')
+    print(g)
+    break
